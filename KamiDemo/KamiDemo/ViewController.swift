1d91b2495656adc9329f0caf7677ea939da33fa2
sschipmunks <leslieZhang@mymm.com>
2017-10-18 12:21:53 +0800

    commit

---
+++ KamiDemo/KamiDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KamiDemo
+//
+//  Created by Leslie Zhang on 2017/10/18.
+//  Copyright © 2017年 Leslie Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
